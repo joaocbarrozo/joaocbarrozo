62cf5be92271c602b2cf9b5e6570568adb0267df
joao.c <joaocbarrozo@gmail.com>
2022-06-17 19:14:15 -0300

    aula 1

---
+++ src/BooleanExpressions.java
@@ -0,0 +1,25 @@
+/**
+ * BooleanExpressions
+ */
+public class BooleanExpressions {
+
+    public static void main(String[] args) {
+        System.out.println(false && false);
+        System.out.println(false && true);
+        System.out.println(true && false);
+        System.out.println(true && true);
+
+        System.out.println(false || false);
+        System.out.println(false || true);
+        System.out.println(true || false);
+        System.out.println(true || true);
+
+        System.out.println(false ^ false);
+        System.out.println(false ^ true);
+        System.out.println(true ^ false);
+        System.out.println(true ^ true);
+
+        // ! > && > ||
+
+    }
+}
---
+++ src/RelationalExpressions.java
@@ -0,0 +1,16 @@
+public class RelationalExpressions {
+    public static void main(String[] args) {
+        System.out.println(1==1);
+        System.out.println(1==2);
+        System.out.println(1!=1);
+        System.out.println(1!=2);
+        System.out.println(1>=1);
+        System.out.println(1>=2);
+        System.out.println(1<=1);
+        System.out.println(1<=2);
+        System.out.println(1>1);
+        System.out.println(1>2);
+        System.out.println(1<1);
+        System.out.println(1<2);
+    }
+}
